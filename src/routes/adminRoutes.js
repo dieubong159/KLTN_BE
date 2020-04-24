52d968c17ab5a30a4936bffba4a5ad0f91cd132c
tannt_t98 <tannt.it98@gmail.com>
2020-04-24 20:35:17 +0700

    get admin not paging and one more api basic

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -7,9 +7,11 @@ const AdminModel = require("../models/Admin");
 
 const Admin = mongoose.model("Admin");
 
-router.post("/admin", async (req, res, next) => {
+router.post("/addadmin", async (req, res, next) => {
     const admin = new Admin(req.body);
-    if (!admin.isModified("password")) return next();
+    if (!admin.isModified("password")) {
+        return next();
+    }
     bcrypt.genSalt(10, function (err, salt) {
         if (err) return next(err);
         bcrypt.hash(admin.password, salt, (err, hash) => {
@@ -33,11 +35,11 @@ router.post("/admin", async (req, res, next) => {
 });
 
 router.get("/admin/CheckUserName", async (req, res) => {
-    var query = { username: req.body.UserName };
+    var query = { username: req.body.username };
     Admin.find(query).then(result => {
         //result = result.toJSON();
         delete result.__v;
-        return res.status(200).send(result);
+        res.status(200).send(result);
     });
 });
 
@@ -50,33 +52,33 @@ router.get("/admin/:admin_id", async (req, res) => {
 });
 
 router.get("/admin", async (req, res) => {
-    let perPage =
-        req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 200;
-    let page = 0;
-    if (req.query) {
-        if (req.body.page) {
-            req.query.page = parseInt(req.query.page);
-            page = Number.isInteger(req.body.page) ? req.body.page : 0;
-        }
-    }
+    // let perPage =
+    //     req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 200;
+    // let page = 0;
+    // if (req.query) {
+    //     if (req.body.page) {
+    //         req.query.page = parseInt(req.query.page);
+    //         page = Number.isInteger(req.body.page) ? req.body.page : 0;
+    //     }
+    // }
     var listAdmin = await Admin.find()
-        .limit(perPage)
-        .skip(perPage * page)
+        // .limit(perPage)
+        // .skip(perPage * page)
 
     res.status(200).send(listAdmin);
 });
 
 router.patch("/admin/:admin_id", async (req, res, next) => {
     var userData = req.body;
-    if (userData.password) {
-        bcrypt.genSalt(10, function (err, salt) {
-            if (err) return next(err);
-            bcrypt.hash(userData.password, salt, (err, hash) => {
-                if (err) return next(err);
-                userData.password = hash;
-            });
-        });
-    }
+    // if (userData.password) {
+    //     bcrypt.genSalt(10, function (err, salt) {
+    //         if (err) return next(err);
+    //         bcrypt.hash(userData.password, salt, (err, hash) => {
+    //             if (err) return next(err);
+    //             userData.password = hash;
+    //         });
+    //     });
+    // }
     Admin.findById(req.params.admin_id, function (err, user) {
         for (let i in userData) {
             user[i] = userData[i];
@@ -95,6 +97,25 @@ router.patch("/admin/:admin_id", async (req, res, next) => {
             }
         );;
     });
+});
+
+router.delete("/admin/:admin_id", async (req, res) => {
+    Admin
+        .findByIdAndRemove(req.params.admin_id)
+        .exec()
+        .then(doc => {
+            if (!doc) { return res.status(404).end(); }
+            return res.status(200).json({
+                message: 'Admin delete successfully!'
+            });
+        })
+        .catch(
+            (error) => {
+                res.status(500).json({
+                    error: error
+                });
+            }
+        );
 });
 
 module.exports = router;
